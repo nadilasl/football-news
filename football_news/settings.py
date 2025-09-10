8117d75252ce8f4a3ad38c66dddd300ff89c2ba7
nadilasl <nadilaslf1974@gmail.com>
2025-09-10 14:50:30 +0700

    benerin csrf

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nadila-salsabila-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://nadila-salsabila-footballnews.pbp.cs.ui.ac.id/"
+    "https://nadila-salsabila-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
